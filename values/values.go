8c49ad6c095a6b03a3825ce656b0387a8bf7c597
anas <anas.hasan@instaview.ai>
2024-06-25 00:37:36 +0530

    values in go

---
+++ values/values.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("int:",34+56)
+	fmt.Println("float:",634/56)
+	fmt.Println("string:","instaview"+" "+"view")
+	fmt.Println("bool:",true&&false)
+}
