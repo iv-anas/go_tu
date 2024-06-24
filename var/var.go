eb916136b3ca3f681598ca7b2307d672e65d1df6
anas <anas.hasan@instaview.ai>
2024-06-25 00:48:19 +0530

    3: var in go

---
+++ var/var.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var v1 string = "hello"
+	fmt.Println(v1)
+	var v2, v3 int = 11, 22
+	fmt.Println(v2 ,v3)
+
+	var var4 int
+	fmt.Println(var4) // deafult value
+
+	var5 :="mostly used"
+	fmt.Println(var5)
+}
+
+// output
+// hello
+// 11 22
+// 0
+// mostly used
