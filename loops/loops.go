1d6c7cf37a222090431c0fca148bf7884b5cda70
anas <anas.hasan@instaview.ai>
2024-06-25 01:11:00 +0530

    5:loops in go

---
+++ loops/loops.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+
+
+func main(){
+	for i := 0; i < 5; i++ {
+        fmt.Println(i)
+    }
+
+	i:=1
+	for i<=5{
+		fmt.Println("loop")
+		i=i+1
+	}
+
+	for i := range 3 {
+        fmt.Println("range", i)
+    }
+}
