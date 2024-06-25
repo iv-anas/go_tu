1330b4247bc80bb6277e5db128a864630bbb2b48
anas <anas.hasan@instaview.ai>
2024-06-25 10:54:09 +0530

    7: switch in go

---
+++ switch/switch.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	t := time.Now()
+
+	switch{
+	case t.Hour() < 12:
+		fmt.Println("good morning")
+	case t.Hour()>12 && t.Hour()<5:
+		fmt.Println("good afternoon")
+	default:
+		fmt.Println("It's good evening")
+	}
+
+	
+
+	this := func(i interface{}) {
+        switch t := i.(type) {
+        case bool:
+            fmt.Println("I'm a bool")
+        case int:
+            fmt.Println("I'm an int")
+        default:
+            fmt.Printf("Don't know type %T\n", t)
+        }
+    }
+	this(true)
+    this(1)
+    this("hey")
+
+}
+
+
+// output
+// good morning
+// I'm a bool
+// I'm an int
+// Don't know type string
