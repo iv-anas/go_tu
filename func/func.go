d75c1c83f1fe3f99da574cd8db1058212c56adc2
anas <anas.hasan@instaview.ai>
2024-06-25 11:48:13 +0530

    11: returning multiple parameters in go

--- func/func.go
+++ func/func.go
@@ -6,6 +6,14 @@ func plus(a, b, c int) int {
 	return a + b + c
 }
 
+func vals() (int, int) {
+    var a,b int
+	fmt.Scanln(&a)
+	fmt.Scanln(&b)
+
+	return a,b
+
+}
 func main() {
 	var a, b, c int
 	fmt.Scanln(&a)
@@ -14,4 +22,8 @@ func main() {
 
 	res:=plus(a,b,c)
 	fmt.Println(res)
+
+	
+	_,e:=vals()
+	fmt.Println(e)
 }
