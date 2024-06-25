360ffd38cdfc5d530d045a1619538f7a45a89ca2
anas <anas.hasan@instaview.ai>
2024-06-25 11:58:09 +0530

    12:closures in go

---
+++ clo/clos.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func intSeq() func() int {
+    i := 0
+    return func() int {
+        i++
+        return i
+    }
+}
+
+func main() {
+
+    nextInt := intSeq()
+
+    fmt.Println(nextInt())
+    fmt.Println(nextInt())
+    fmt.Println(nextInt())
+
+    newInts := intSeq()
+    fmt.Println(newInts())
+	fmt.Println(nextInt())
+}
+
+// output
+// 1
+// 2
+// 3
+// 1
+// 4
